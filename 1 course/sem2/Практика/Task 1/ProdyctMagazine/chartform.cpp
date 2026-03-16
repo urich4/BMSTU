bd4f44bd517884040807bee9207bcd2a39e1644e
urich4 <sesobol63@gmail.com>
2026-03-16 12:18:46 +0300

    new

--- 1 course/sem2/Практика/Task 1/ProdyctMagazine/chartform.cpp
+++ 1 course/sem2/Практика/Task 1/ProdyctMagazine/chartform.cpp
@@ -1,17 +1,14 @@
 #include "chartform.h"
 
 chartForm::chartForm(const QString &productName, const QMap<QDate, int> &chartData) {
-    // 1. Создаем строку ввода и кнопку
-    productInput = new QLineEdit(productName); // Сразу вписываем имя текущего товара
+    productInput = new QLineEdit(productName);
     buildButton = new QPushButton("Показать график");
     connect(buildButton, &QPushButton::clicked, this, &chartForm::onButtonClicked);
 
-    // 2. Создаем верхнюю горизонтальную панель и кладем туда наши виджеты
     QHBoxLayout *topPanel = new QHBoxLayout();
     topPanel->addWidget(productInput);
     topPanel->addWidget(buildButton);
 
-    // 3. Строим сам график (это твой готовый код)
     QChart *chart = new QChart();
     chart->legend()->hide();
     QLineSeries *series = new QLineSeries();
@@ -40,28 +37,25 @@ chartForm::chartForm(const QString &productName, const QMap<QDate, int> &chartDa
     chart->addAxis(axisY, Qt::AlignLeft);
     series->attachAxis(axisY);
 
-    // Оборачиваем готовый chart в QChartView
+
     chartView = new QChartView(chart);
 
-    // 4. Создаем главный вертикальный Layout для всего окна
-    QVBoxLayout *mainLayout = new QVBoxLayout(this); // this означает, что layout ставится прямо в наше окно
-    mainLayout->addLayout(topPanel);    // Сначала ставим верхнюю панель
-    mainLayout->addWidget(chartView);   // Под ней размещаем график
 
-    // 5. Настройки самого окна
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->addLayout(topPanel);
+    mainLayout->addWidget(chartView);
+
     this->setWindowFlags(Qt::Window);
     this->resize(800, 600);
     this->setAttribute(Qt::WA_DeleteOnClose);
 }
 
 void chartForm::onButtonClicked() {
-    // Берем текст из поля ввода и отправляем сигнал главному окну
     QString newProduct = productInput->text();
     emit requestNewData(newProduct);
 }
 
 void chartForm::updateChart(const QString &productName, const QMap<QDate, int> &chartData) {
-    // Создаем абсолютно чистый холст
     QChart *newChart = new QChart();
     newChart->legend()->hide();
 
@@ -89,7 +83,5 @@ void chartForm::updateChart(const QString &productName, const QMap<QDate, int> &
     axisY->setLabelFormat("%i");
     newChart->addAxis(axisY, Qt::AlignLeft);
     series->attachAxis(axisY);
-
-    // Подменяем старый график на новый. Старый удалится из памяти сам.
     chartView->setChart(newChart);
 }
